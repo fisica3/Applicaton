0e5a0194bed8980ec7569039a5909852c065887f
Ernesto <ecardenas@rocketmail.com>
2013-11-20 09:45:41 -0500

    Agregando Pruebas unitarias

---
+++ Matematicas/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matematicas
+{
+    public class Basicas
+    {
+        public int Fibonacci(int numero)
+        {
+            return 0;
+        }
+
+    }
+}
