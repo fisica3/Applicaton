3001b65d09989bad0cae93d6fd6ac21c9ec5aac1
Ernesto <ecardenas@rocketmail.com>
2013-11-21 15:05:14 -0500

    Agregando Tests NUnit

---
+++ TestNUnit/TestAlterno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestNUnit
+{
+    public class TestAlterno
+    {
+        //Otro mensaje :D
+        [Test]
+        public void TestFibonacciValorInicial()
+        {
+            var sut = new Matematicas.Basicas();
+            Assert.AreEqual(1, sut.Fibonacci(1), "Error 0");
+        }
+    }
+}
