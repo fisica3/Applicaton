e7ba555803d53612c5af12b43e63020057efc448
Ernesto <ecardenas@rocketmail.com>
2013-11-21 15:13:15 -0500

    Detalle Test NUnit

--- TestNUnit/TestAlterno.cs
+++ TestNUnit/TestAlterno.cs
@@ -16,5 +16,38 @@ namespace TestNUnit
             var sut = new Matematicas.Basicas();
             Assert.AreEqual(1, sut.Fibonacci(1), "Error 0");
         }
+
+        [Test]
+        public void TestFibonacciValor2()
+        {
+            var sut = new Matematicas.Basicas();
+            Assert.AreEqual(1, sut.Fibonacci(2), "Error 2");
+        }
+        [Test]
+        public void TestFibonacciValor3()
+        {
+            var sut = new Matematicas.Basicas();
+            Assert.AreEqual(2, sut.Fibonacci(3), "Error 3");
+        }
+        [Test]
+        public void TestFibonacciValor4()
+        {
+            var sut = new Matematicas.Basicas();
+            Assert.AreEqual(3, sut.Fibonacci(4), "Error 4");
+        }
+
+        [Test]
+        public void TestFibonacciValor5()
+        {
+            var sut = new Matematicas.Basicas();
+            Assert.AreEqual(5, sut.Fibonacci(5), "Error 5");
+        }
+
+        [Test]
+        public void TestFibonacciValor6()
+        {
+            var sut = new Matematicas.Basicas();
+            Assert.AreEqual(8, sut.Fibonacci(6), "Error 5");
+        }
     }
 }
